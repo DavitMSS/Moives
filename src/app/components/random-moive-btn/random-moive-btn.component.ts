c2112bb889df50bc0a7eeef97002ff7c664f3271
DavitMSS <davit.beridze@makingscience.com>
2023-11-29 19:09:34 +0400

    create random movie generator button

---
+++ src/app/components/random-moive-btn/random-moive-btn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-random-moive-btn',
+  templateUrl: './random-moive-btn.component.html',
+  styleUrls: ['./random-moive-btn.component.css']
+})
+export class RandomMoiveBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
